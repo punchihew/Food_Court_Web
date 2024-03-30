15bf81dc13101aac1c023b9afd22e98fd72dae69
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-30 11:26:11 +0530

    update

--- Assept/js/slider/main.js
+++ Assept/js/slider/main.js
@@ -6,3 +6,12 @@ const images = Array.from(slider.children);
 
 // Set the initial image index
 let imageIndex = 0;
+
+function updateSlider() {
+    // Remove the 'active', 'previous', 'next', and 'inactive' classes from all images
+    images.forEach(image => {
+        image.classList.remove('active', 'previous', 'next', 'inactive');
+    });
+
+    // Add the 'active' class to the current image
+    images[imageIndex].classList.add('active');
