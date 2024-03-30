86d4ae34ce3cbf22d81066052a12e56e08e28bf6
punchihewa <navishkapunchihewa66@gmail.com>
2024-03-30 11:34:04 +0530

    update

--- Assept/js/slider/main.js
+++ Assept/js/slider/main.js
@@ -36,3 +36,26 @@ function updateSlider() {
             image.classList.add('inactive');
         }
     });
+
+    // Set the opacity of all the background divs to 0
+    backgrounds.forEach((background) => {
+        background.style.opacity = 0;
+    });
+    // If the current image is active, set the opacity of the corresponding background div to 1
+    if (images[imageIndex].classList.contains('active')) {
+        backgrounds[imageIndex].style.opacity = 1;
+    }
+    // Update the image index
+    imageIndex = (imageIndex + 1) % images.length;
+}
+
+updateSlider();
+// Update the slider every 3 seconds
+setInterval(updateSlider, 3000);
+
+images[1].classList.add('next');
+images[2].classList.add('inactive');
+images[3].classList.add('inactive');
+images[4].classList.add('previous');
+images[0].classList.add('active');
+
