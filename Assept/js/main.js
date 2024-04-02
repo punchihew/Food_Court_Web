763e89b09cb3911e8aae979aed64ddae59f75728
punchihewa <navishkapunchihewa66@gmail.com>
2024-04-02 12:15:10 +0530

    update

---
+++ Assept/js/main.js
@@ -0,0 +1,30 @@
+
+
+
+(function ($) {
+    "use strict";
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+})(jQuery);
+
