f881a60d0a203c148c4be048602e94ad81edc347
punchihewa <navishkapunchihewa66@gmail.com>
2024-04-02 20:38:13 +0530

    update

--- Assept/js/main1.js
+++ Assept/js/main1.js
@@ -43,5 +43,11 @@
       delay: 5000,
       disableOnInteraction: false
     },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
 
 })()
